e39920dc0d78d3f97743bd2eccfee115eb6a6e39
phattharawadi18 <s6102041610059@email.kmutnb.ac.th>
2021-08-03 16:57:50 +0700

    เพิ่มfunction

---
+++ dart/function.dart
@@ -0,0 +1,32 @@
+void main() {
+   callWeb("Yahoo",port:"443",protocal: "http");
+  }
+  void callWeb(String web,{String protocal="http", String port = "80"}) => print("$web $protocal $port");
+
+  int callColor([String newColor = "pink"]){
+
+  List<dynamic> colors = ["red","blue","green"];
+
+  
+
+  for(var i in colors){
+    print(i);
+  }
+
+  return colors.length;
+}
+
+
+String callGen(int year) {
+  print(year);
+/*
+  if (year >= 1996){
+    print("Gen Z");
+  }else {
+    print("Gen Y or Upper");
+  }
+*/
+// เช็คเงื่อนไข        ถ้าเป็นจริง       ถ้าเป็นเท็จ
+  var  gen = (year >= 1996) ? "Gen Z" : "Gen Y or Upper" ;
+  return gen;
+}
